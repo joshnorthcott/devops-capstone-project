b81fbbc7d46a4fc1167a4ea5ea313fb92d99d9a1
joshnorthcott <joshnorthcott@hotmail.com>
2024-11-21 18:21:59 -0500

    this has to work

--- service/routes.py
+++ service/routes.py
@@ -83,10 +83,7 @@ main
 
 list-function
 @app.route("/accounts/<int:account_id>", methods=["GET"])
-
-app.route("/accounts/<int:account_id>", methods=["GET"])
-main
-    def get_accounts(account_id):
+def get_accounts(account_id):
         """
         Reads an Account
         This endpoint will read an Account based the account_id that is requested
